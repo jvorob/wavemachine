cba2d058788a95252c7c8cbcf3b72b3a9feb103f
George Vorobyev <quaffle97@gmail.com>
2014-10-05 01:15:29 -0400

    Added simple ui and toolbar, can click on grid to add/remove water

--- main.c
+++ main.c
@@ -7,32 +7,76 @@
 
 #define pi 3.14159265358979323
 
+#define WIDTH 10
+#define HEIGHT 10
+#define CELLSIZE 32
+
 SDL_Renderer *setupWindow();
 Uint32 timercallback(Uint32 interval, void *param);
 void cleanup();
-void draw(SDL_Renderer *r, sim_Sim *s);
+void drawGrid(SDL_Renderer *r, sim_Sim *s, SDL_Rect *viewport);
+void drawTools(SDL_Renderer *r, int selected, int hovered, SDL_Rect *viewport);
+void doMouseEvent(SDL_Event *e, int *selected, int *hovered, sim_Sim *s);
+void printGrid(sim_Sim *s);
+
+int SDL_PointInRect(SDL_Point *p, SDL_Rect *r) {
+	return (int)(p->x >= r->x && p->y >= r->y && p->x <= r->x + r->w && p->y <= r->y + r->h);
+}
+
+struct timerparams {
+	int CustomTimerEvent;
+	int timerlock;
+};
 
 
-SDL_Renderer *ren;
-int CustomTimerEvent;
-int timerlock = 0;//if 1, dont send timerevents
 
-int main(int argc, char **argv){
+SDL_Texture *iconText;
+SDL_Rect gridviewport, toolbox;
+
+int main(int argc, char *argv[]){
+	struct timerparams tparams;
+	SDL_Event e;
+	sim_Sim *mysim;
+	SDL_Renderer *ren;
+	int temp;
+	SDL_Surface *iconSurf;
+	SDL_TimerID timerid;
+	int selected, hovered;
+
+	selected = 0;
+	hovered = -1;
+
+	gridviewport.x = 100;
+	gridviewport.y = 0;
+	gridviewport.w = WIDTH * CELLSIZE;
+	gridviewport.h = HEIGHT * CELLSIZE;
+
+	toolbox.x = 500;
+	toolbox.y = 0;
+	toolbox.w = 100;
+	toolbox.h = 400;
+
 	ren = setupWindow();
 
+	SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_BLEND);
 
-	if((CustomTimerEvent = SDL_RegisterEvents(1)) == -1){
+	iconSurf = SDL_LoadBMP("icons.bmp");
+	iconText = SDL_CreateTextureFromSurface(ren, iconSurf);
+
+	if((tparams.CustomTimerEvent = SDL_RegisterEvents(1)) == -1){
 		printf("Failed to register customevent\n");
 		exit(-1);
 	}
+	tparams.timerlock = 0;
 
-	SDL_AddTimer(100, timercallback, NULL);
+	if(timerid = SDL_AddTimer(1000, timercallback, (void *) &tparams))
+		;
+	else
+		fprintf(stderr,"Timer failed to start\n");
 
-	int quit = 0;
-	SDL_Event e;
 
-	int WIDTH = 10, HEIGHT = 10;
-	sim_Sim *mysim = sim_CreateSimulation(WIDTH, HEIGHT, 50.0);
+	mysim = sim_CreateSimulation(WIDTH, HEIGHT, 50.0);
+
 	mysim->curr[0].height = 256;
 	mysim->curr[1].height = 256;
 	mysim->curr[2].height = 256;
@@ -47,39 +91,75 @@ int main(int argc, char **argv){
 	mysim->curr[WIDTH * HEIGHT - 5].height = 256;
 	mysim->curr[WIDTH * HEIGHT - 6].height = 256;
 
-	int temp;
+
+	int quit = 0;
 
 	while (!quit) {
-		while ((temp = SDL_PollEvent(&e)) != 0) {
-
-			if(e.type == SDL_QUIT) {
-				quit = 1;
-				break;
-			} else if(e.type == SDL_USEREVENT) {
-				draw(ren, mysim);
-				sim_step(mysim);
-				timerlock = 0;
+		drawGrid(ren, mysim, &gridviewport);
+		drawTools(ren, selected, hovered, &toolbox);
+		SDL_RenderPresent(ren);
+		while ((temp = SDL_PollEvent(&e)) != 0 && !quit) {
+			switch (e.type) {
+				case SDL_QUIT:
+					quit = 1;
+					break;
+				case SDL_USEREVENT:
+					sim_step(mysim);
+					printGrid(mysim);
+					tparams.timerlock = 0;
+					break;
+				case SDL_MOUSEMOTION:
+				case SDL_MOUSEBUTTONDOWN:
+				case SDL_MOUSEBUTTONUP:
+					doMouseEvent(&e, &selected, &hovered, mysim);
+					break;
+				case SDL_WINDOWEVENT:
+					switch (e.window.event) {
+						case SDL_WINDOWEVENT_ENTER:
+						case SDL_WINDOWEVENT_LEAVE:
+							doMouseEvent(&e, &selected, &hovered, mysim);
+							break;
+					}
+					break;
+				default:
+					fprintf(stderr, "UNKNOWN EVENT TYPE\n");
+					break;
 			}
 		}
 	}
-
 
 	cleanup();
 	return 0;
 }
 
-void draw(SDL_Renderer *r, sim_Sim *s) {
+
+void printGrid(sim_Sim *s) {
 	int i, j;
-	int rectwidth = 32, rectheight = 32;
 	int debugprint = 1;
+	sim_Cell *cell;
+
+	for(i = 0; i < s->h; i++) {
+		for(j = 0; j < s->w; j++) {
+			cell = &(cellAt(j,i,s));
+			if(debugprint) fprintf(stderr,"%4.0lf ", cell->height);
+		}
+		if(debugprint) fprintf(stderr,"\n");
+	}	
+	if(debugprint) fprintf(stderr, "\nAvgheight:%lf\n---\n\n", sim_avgheight(s));
+	if(debugprint) fprintf(stderr, "\n---\n\n");
+}
+
+void drawGrid(SDL_Renderer *r, sim_Sim *s, SDL_Rect *viewport) {
+	int i, j;
 	int drawflow = 1;
 	sim_Cell *cell;
 	SDL_Rect rect;
-	rect.w = rectwidth;
-	rect.h = rectheight;
+	rect.w = CELLSIZE;
+	rect.h = CELLSIZE;
 
 	SDL_SetRenderDrawColor(r, 200, 255, 200, 255);
 	SDL_RenderClear(r);
+
 
 	for(i = 0; i < s->h; i++) {
 		for(j = 0; j < s->w; j++) {
@@ -87,73 +167,156 @@ void draw(SDL_Renderer *r, sim_Sim *s) {
 
 			int rg = 255 - (int)cell->height;
 			rg = rg < 0 ? 0 : rg;
-			rect.x = j * rectwidth;
-			rect.y = i * rectheight;
+			rect.x = j * CELLSIZE + viewport->x;
+			rect.y = i * CELLSIZE + viewport->y;
 
-			SDL_SetRenderDrawColor(ren, rg, rg, 255, 255); 
-			SDL_RenderFillRect(ren, &rect);
+			SDL_SetRenderDrawColor(r, rg, rg, 255, 255); 
+			SDL_RenderFillRect(r, &rect);
 
-			SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
+			SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
 
 
-			if(debugprint) fprintf(stderr,"%4.0lf ", cell->height);
 		}
-		if(debugprint) fprintf(stderr,"\n");
 	}	
-	if(debugprint) fprintf(stderr, "\nAvgheight:%lf\n---\n\n", sim_avgheight(s));
-	if(debugprint) fprintf(stderr, "\n---\n\n");
-
 	if(drawflow)
 		for(i = 0; i < s->h; i++) {
 			for(j = 0; j < s->w; j++) {
 				cell = &(cellAt(j,i,s));
-				rect.x = j * rectwidth;
-				rect.y = i * rectheight;
+				rect.x = j * CELLSIZE + viewport->x;
+				rect.y = i * CELLSIZE + viewport->y;
 				int len;
 				len = cell->flow[RT];
-				SDL_RenderDrawLine(ren, 
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2,
-					rect.x + rectwidth / 2 + len,
-					rect.y + rectheight / 2);
+				SDL_RenderDrawLine(r, 
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2,
+					rect.x + CELLSIZE / 2 + len,
+					rect.y + CELLSIZE / 2);
 				len = cell->flow[LF];
-				SDL_RenderDrawLine(ren, 
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2,
-					rect.x + rectwidth / 2 - len,
-					rect.y + rectheight / 2);
+				SDL_RenderDrawLine(r, 
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2,
+					rect.x + CELLSIZE / 2 - len,
+					rect.y + CELLSIZE / 2);
 				len = cell->flow[DN];
-				SDL_RenderDrawLine(ren, 
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2,
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2 + len);
+				SDL_RenderDrawLine(r, 
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2,
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2 + len);
 				len = cell->flow[UP];
-				SDL_RenderDrawLine(ren, 
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2,
-					rect.x + rectwidth / 2,
-					rect.y + rectheight / 2 - len);
+				SDL_RenderDrawLine(r, 
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2,
+					rect.x + CELLSIZE / 2,
+					rect.y + CELLSIZE / 2 - len);
 			}
 		}
+}
 
-	SDL_RenderPresent(r);
+void drawTools(SDL_Renderer *r, int selected, int hovered, SDL_Rect *viewport){
+	SDL_Rect temp;
+	temp.x = 0;//copy from origin on src texture
+	temp.y = 0;
+	temp.w = viewport->w;
+	temp.h = viewport->h;
+	SDL_RenderCopy(r, iconText, &temp, viewport);
+
+	if(hovered != -1) {//Draw hover rectangle
+		temp.x = viewport->x;
+		temp.y = viewport->y;
+		temp.w = 100;
+		temp.h = 100;
+
+		temp.y += 100 * hovered;
+
+		SDL_SetRenderDrawColor(r, 255, 0, 0, 255);
+		SDL_RenderDrawRect(r, &temp);
+	}
+
+	temp.x = viewport->x;
+	temp.y = viewport->y;
+	temp.w = 100;
+	temp.h = 100;
+
+	temp.y += 100 * selected;
+
+	SDL_SetRenderDrawColor(r, 0, 0, 255, 100);
+	SDL_RenderFillRect(r, &temp);
+}
+
+void doMouseEvent(SDL_Event *e, int *selected, int *hovered, sim_Sim *s) {
+	static int laststate, clicked;
+	Uint32 buttons;
+	SDL_Point loc;
+	
+	buttons = SDL_GetMouseState(&(loc.x), &(loc.y));
+	clicked = laststate & SDL_BUTTON(SDL_BUTTON_LEFT) && !(buttons & SDL_BUTTON(SDL_BUTTON_LEFT));
+
+	switch (e->type) {
+		case SDL_WINDOWEVENT:
+			*hovered = -1;
+			if(e->window.event == SDL_WINDOWEVENT_LEAVE) {
+				loc.x = loc.y = -1;//Mouse is outside the window
+			}
+				break;
+		case SDL_MOUSEMOTION:
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+			break;
+		default:
+			break;
+	}
+
+	*hovered = -1;
+	if(SDL_PointInRect(&loc, &toolbox)) {
+		*hovered = (loc.y - toolbox.y) / 100;
+		if(buttons & SDL_BUTTON(SDL_BUTTON_LEFT))
+			*selected = *hovered;
+		//Each button in the toolbox is 100px high
+		
+	} else if(SDL_PointInRect(&loc, &gridviewport)) {
+		switch(*selected) {
+			case 0: //Add water
+				if(!clicked)
+					break;
+				loc.x -= gridviewport.x;
+				loc.y -= gridviewport.y;
+				loc.x /= CELLSIZE;
+				loc.y /= CELLSIZE;
+				cellAt(loc.x, loc.y, s).height += 30;
+				break;
+			case 1:
+				if(!clicked)
+					break;
+				loc.x -= gridviewport.x;
+				loc.y -= gridviewport.y;
+				loc.x /= CELLSIZE;
+				loc.y /= CELLSIZE;
+				cellAt(loc.x, loc.y, s).height -= 30;
+				//Remove water
+
+				break;
+		}
+	} else {
+		
+	}
+
+	laststate = buttons;
 }
 
 void cleanup() {
-	SDL_DestroyRenderer(ren);
 	SDL_Quit();
 }
 
 Uint32 timercallback(Uint32 interval, void *param) {
-
+	struct timerparams *tparams = (struct timerparams *) param;
 	SDL_Event event;
 	SDL_zero(event);
-	event.type = CustomTimerEvent;
+	event.type = tparams->CustomTimerEvent;
 
-	if(!timerlock){
+	if(!tparams->timerlock){
 		SDL_PushEvent(&event);
-		timerlock = 1;
+		tparams->timerlock = 1;
 	}
 
 	return interval;
